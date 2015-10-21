090863df394edf6b7b161ded04851bcde8b26b21
Karel Seeuwen (work) <karepon@gmail.com>
2015-10-21 12:05:06 +0900

    Removed Dummy project

--- vs2012_nRf_dfu/vs2012_nRf_dfu/stdafx.cpp
+++
@@ -1,7 +0,0 @@
-// stdafx.cpp : source file that includes just the standard includes
-// vs2012_nRf_dfu.pch will be the pre-compiled header
-// stdafx.obj will contain the pre-compiled type information
-
-#include "stdafx.h"
-
-
